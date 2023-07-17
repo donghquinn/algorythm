d16e1ef44d936243811c579e4aa11d1641a15f03
Kim Donghyun <kimdonghyun@Daniel-Quinns-Mac-Book.local>
2023-07-17 14:58:35 +0900

    view

--- linked.go
+++ linked.go
@@ -1,5 +1,7 @@
 package linked
 
+import "reflect"
+
 type LinkedList struct {
 	Header *Node
 	Tail   *Node
@@ -11,10 +13,39 @@ type Node struct {
 	Value interface{}
 }
 
+// 생성
 func NewLinkedList() *LinkedList {
 	return &LinkedList{nil, nil, 0}
 }
 
 func NewNode(v interface{}) *Node {
 	return &Node{nil, v}
+}
+
+// 조회
+func (link *LinkedList) FindByValue(value interface{}) *Node {
+	for n := link.Header; n != nil; {
+		if reflect.DeepEqual(n.Value, value) {
+			return n
+		} else {
+			n = n.Next
+		}
+	}
+
+	return nil
+}
+
+func (link *LinkedList) IndexOf(i int) *Node {
+	node := link.Header
+	if node == nil {
+		return nil
+	}
+	for n := 0; n < i; n++ {
+		if node == nil {
+			return nil
+		} else {
+			node = node.Next
+		}
+	}
+	return node
 }
